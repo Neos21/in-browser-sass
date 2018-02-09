cd3b5cb844a8a6c865e6835b0b63cabffd4c2c1e
Neos21 <neos21@gmail.com>
2018-02-09 15:02:49 +0900

    Modify build script

--- scripts/build-standalone.js
+++ scripts/build-standalone.js
@@ -1,19 +1,16 @@
 const fs = require('fs');
 
 // Sass.js
-const sass = fs.readFileSync('./src/sass-0.10.7.sync.min.js');
-const sassTxt = sass.toString();
+const sass = fs.readFileSync('./src/sass-0.10.7.sync.min.js', 'utf-8');
 
 // Normal Version
-const src = fs.readFileSync('./src/in-browser-sass-standalone.js');
-const srcTxt = src.toString();
+const src = fs.readFileSync('./src/in-browser-sass-standalone.js', 'utf-8');
 
-const distTxt = srcTxt.replace('/*! Sass.js */', sassTxt);
-fs.writeFileSync('./in-browser-sass-standalone.js', distTxt);
+const dist = src.replace('/*! Sass.js */', sass);
+fs.writeFileSync('./in-browser-sass-standalone.js', dist);
 
 // Uglify Version : Uglify 後に Sass.js を追加する
-const uglifiedSrc = fs.readFileSync('./in-browser-sass-standalone.min.js');
-const uglifiedSrcTxt = uglifiedSrc.toString();
+const uglifiedSrc = fs.readFileSync('./in-browser-sass-standalone.min.js', 'utf-8');
 
-const uglifiedDistTxt = uglifiedSrcTxt.replace('/*! Sass.js */', sassTxt);
-fs.writeFileSync('./in-browser-sass-standalone.min.js', uglifiedDistTxt);
+const uglifiedDist = uglifiedSrc.replace('/*! Sass.js */', sass);
+fs.writeFileSync('./in-browser-sass-standalone.min.js', uglifiedDist);
